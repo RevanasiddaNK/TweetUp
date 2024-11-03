d3023f5f379522dcaf5f46f15e46e64fbc9cacfe
RevanasiddaNK <nmrevanasiddarhbk@gmail.com>
2024-11-04 04:25:09 +0530

    final deployment setup

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,14 @@ app.use("/api/notification", notificationRoute);
 const port = process.env.PORT || 8080;
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    // Serve static files from the frontend build folder
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    });
 }
+
 
 // Start server
 
